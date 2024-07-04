13605f300eb7b97426379c62d92f5a5d18b8ed64
Alex Kozik <ajk333@cornell.edu>
2024-07-04 16:07:16 -0400

    backup

--- spamfilter.py
+++ spamfilter.py
@@ -16,7 +16,7 @@ print("finished loading word_to_index")
 index_to_word = json.load(open("index_to_word.json"))
 print("finished loading index_to_word")
 
-training_data = json.load(open("training_data_subset.json"))
+training_data = json.load(open("training_data.json"))
 print("finished loading training_data")
 
 # convert the data to tensors
@@ -42,9 +42,8 @@ print("finished loading words")
 class NN(nn.Module):
   def __init__(self):
     super(NN, self).__init__()
-    self.fc1 = nn.Linear(len(words), 128)
-    self.fc2 = nn.Linear(128, 64)
-    self.fc3 = nn.Linear(64, 1)
+    self.fc1 = nn.Linear(len(words), 100)
+    self.fc2 = nn.Linear(100, 1)
     self.relu = nn.ReLU()
     self.sigmoid = nn.Sigmoid()
 
@@ -52,12 +51,10 @@ class NN(nn.Module):
     x = self.fc1(x)
     x = self.relu(x)
     x = self.fc2(x)
-    x = self.relu(x)
-    x = self.fc3(x)
     x = self.sigmoid(x)
     return x
 
-num_epochs = 100
+num_epochs = 10000000000000000000000000
 
 def train_model():
   print("training model")
@@ -67,7 +64,7 @@ def train_model():
   model = torch.load("model.pth")
 
   criterion = nn.BCELoss()
-  optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+  optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
 
   # compute 10%, .... 90% of the dataset indices
   ten = len(training_data) // 10
@@ -75,25 +72,29 @@ def train_model():
 
 
   for epoch in range(num_epochs):
-    total_loss = 0
+    num_misclassifications = 0
     for i in range(len(training_data)):
       sentence, label = training_data[i]
       optimizer.zero_grad()
       output = model(sentence)
+
+      if output.item() >= 0.5 and label.item() == 0:
+        num_misclassifications += 1
+      elif output.item() < 0.5 and label.item() == 1:
+        num_misclassifications += 1
+
       loss = criterion(output, label)
       loss.backward()
       optimizer.step()
-      total_loss += loss.item()
       # every 10% through the dataset, print a message
       if i in printing_points:
         print(f"{i / len(training_data) * 100}% done with epoch {epoch + 1}")
       
     print("Epoch: ", epoch + 1)
-    print("Loss: ", total_loss)
+    print(f"Misclassifications: {num_misclassifications} out of {len(training_data)}")
     torch.save(model, "model.pth")
   
   # save the model
-
 
 def make_model():
   print("making model")
@@ -102,6 +103,10 @@ def make_model():
 
 def validate_model(model):
   correct = 0
+  correct_spam = 0
+  total_spam = 0
+  correct_not_spam = 0
+  total_not_spam = 0
   total = 0
   for sentence, label in testing_data:
     output = model(sentence)
@@ -109,11 +114,25 @@ def validate_model(model):
     label = label.item()
     if output == label:
       correct += 1
+    if label == 1:
+      total_spam += 1
+      if output == label:
+        correct_spam += 1
+    else:
+      total_not_spam += 1
+      if output == label:
+        correct_not_spam += 1
     total += 1
 
   print("Accuracy: ", correct / total)
   print("Correct: ", correct)
   print("Total: ", total)
+  print("Spam Accuracy: ", correct_spam / total_spam)
+  print("Correct Spam: ", correct_spam)
+  print("Total Spam: ", total_spam)
+  print("Not Spam Accuracy: ", correct_not_spam / total_not_spam)
+  print("Correct Not Spam: ", correct_not_spam)
+  print("Total Not Spam: ", total_not_spam)
 
 def test_model():
   model = torch.load("model.pth")
@@ -131,4 +150,4 @@ def test_model():
     prediction_string = "spam" if prediction.item() > 0.5 else "not spam"
     print(f"The sentence is {prediction_string}")
 
-test_model()
+train_model()
